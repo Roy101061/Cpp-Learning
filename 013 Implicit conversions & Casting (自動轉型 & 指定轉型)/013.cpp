40378f1a86cb126009d4715cd401601c781be987
Roy101061 <Roy101061@gmail.com>
2021-12-01 08:14:51 +0000

    update

---
+++ 013 Implicit conversions & Casting (自動轉型 & 指定轉型)/013.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	/*
+	int a = 3.5;  => a = 3
+	cout<<a<<endl;
+	return 0;
+	*/
+	
+	/*
+	int a = 3 * 3 * 3.14159;  => 3*3*3.14159 => a = 28
+	cout<<a<<endl;
+	return 0;
+	*/
+	
+	
+}
