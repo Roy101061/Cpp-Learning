ef8451a9df93b18cd70c5bf5771ad776e94f13a0
Roy101061 <Roy101061@gmail.com>
2021-12-08 08:13:56 +0000

    update

---
+++ 016 Statement 3 if else/016.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int a,b;
+	cout<<"Please enter a number => ";
+	cin>>a;
+	cout<<"Please enter another number => ";
+	cin>>b;
+	if(a>b)
+		cout<<a<<" > "<<b<<endl;
+	if(a<b)
+		cout<<a<<" < "<<b<<endl;
+		
+	/*
+	if(a=b)  //wrong command
+		cout<<a<<" = "<<b<<endl;  // => a=b, if b=0 => true ; if b!=0 => false
+	*/
+	
+	if(a==b)
+		cout<<a<<" = "<<b<<endl;
+	
+	return 0;
+}
