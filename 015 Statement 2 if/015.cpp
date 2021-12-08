716f07eb24fbf2f2b8112cd9a951b2f01044bb16
Roy101061 <Roy101061@gmail.com>
2021-12-08 08:05:11 +0000

    update

---
+++ 015 Statement 2 if/015.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	/*
+	if(false)
+		cout<<"1\n";
+	if(1)
+		cout<<"2\n";
+	*/
+	
+	/*
+	int a,b;
+	cin>>a>>b;
+	if(a>b)
+		cout<<a<<" > "<<b;
+	*/
+	
+	/*
+	bool a = true, b = false;
+	if(a)
+		cout<<"true!\n";
+	if(!b)
+		cout<<"false\n";
+	*/
+	
+	/*
+	int a = 10, b = 20;
+	if(a > b)
+		cout<<"Hi\n";
+		cout<<"Hello\n";
+	//if over 2 commands, use {}
+	*/
+	
+	int a = 10, b = 20;
+	if(a < b){
+		cout<<"Hi\n";
+		cout<<"Hello\n";
+	}
+	
+	return 0;
+}
