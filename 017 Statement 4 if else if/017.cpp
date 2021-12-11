a2d737d85c220ce6192a3608a886bb887c4f6fde
Roy101061 <Roy101061@gmail.com>
2021-12-11 13:49:21 +0000

    update

--- 017 Statement 4 if else if/017.cpp
+++ 017 Statement 4 if else if/017.cpp
@@ -1,17 +1,2 @@
-#include <iostream>
-using namespace std;
-int main(){
-   int a,b;
-   cin>>a>>b;
-   if(a>b){
-      cout<<a<<" > "<<b;
-   }
-   else if(a<b){
-      cout<<a<<" < "<<b;
-   }
-   else{
-      cout<<a<<" = "<<b;
-   }
-   return 0;
-
-}
+if(c == 0)
+		cout<<"Same\n";
