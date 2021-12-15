24bfb76facfda5be445a5a1cad33431d4a65b3be
Roy101061 <Roy101061@gmail.com>
2021-12-15 08:12:42 +0000

    update

---
+++ 019 Statement 2-5 if/019.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	/*
+	cout<<"Please enter a number => ";
+	int num;
+	cin>>num;
+	
+	int a;
+	
+	//if(num==0)
+	//	a=0;
+	//if(num%2==1)
+	//	a=1;
+	//if(num%2==0)
+	//	a=2;
+	//  //¡ô Wrong order
+	
+	if(num%2==1)
+		a=1;
+	if(num%2==0)
+		a=2;
+	if(num==0)
+		a=0;
+	
+	if(a==0)
+		cout<<"0\n";
+	if(a==1)
+		cout<<"Odd\n";
+	if(a==2)
+		cout<<"Even/n";
+	*/
+	
+	/*
+	cout<<"Please enter a number => ";
+	int num;
+	cin>>num;
+	
+	int a=0;
+	
+	if(num%2==1)
+		a=1;
+	if(num%2==0)
+		a=2;
+	
+	if(a==0)
+		cout<<"0\n";
+	if(a==1)
+		cout<<"Odd\n";
+	if(a==2)
+		cout<<"Even/n";
+	*/  //¡ô Wrong order
+	
+	cout<<"Please enter a number => ";
+	int num;
+	cin>>num;
+	
+	int a=2;
+	
+	if(num%2==1)
+		a=1;
+	if(num==0)
+		a=0;
+	
+	if(a==0)
+		cout<<"0\n";
+	if(a==1)
+		cout<<"Odd\n";
+	if(a==2)
+		cout<<"Even/n";
+	
+	return 0;
+}
