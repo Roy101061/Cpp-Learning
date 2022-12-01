a1946a1cd97faed332fe8ae137252bb83843b6a3
Roy101061 <86598941+Roy101061@users.noreply.github.com>
2022-12-01 14:46:18 +0800

    i706 TLE solved

--- 111sem1/intramual competitpon/i706/i706.cpp
+++ 111sem1/intramual competitpon/i706/i706.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main()
 {
+	std::ios_base::sync_with_stdio(false);  // 關閉cin & stdin同步, 減少程式負擔
+	std::cin.tie(0);
+	
     int n;
     while(cin>>n){
         int high[n-1];
@@ -39,3 +42,4 @@ int main()
 
     return 0;
 }
+
